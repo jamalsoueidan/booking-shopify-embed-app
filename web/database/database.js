5f963cbd94002e2e5422501db4f234110ed2eaea
Jamal Soueidan <jamal@soueidan.com>
2022-11-12 19:37:21 +0300

    update database to use env mongo_uri

--- web/database/database.js
+++ web/database/database.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 export const connect = async () => {
   try {
-    await mongoose.connect(
-      "mongodb://127.0.0.1:27017/book-appointment-app?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.0"
-    );
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connecting to MongoDB Atlas cluster...");
   } catch (error) {
     console.error("Connection to MongoDB Atlas failed!", error);
