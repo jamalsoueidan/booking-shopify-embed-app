e3eadf212096defa21854ec89bc25ffb54540c98
Jamal Soueidan <jamal@soueidan.com>
2022-11-25 21:42:12 +0300

    fix cart update webhook

--- web/libs/webhooks/cart/cart.webhook.ts
+++ web/libs/webhooks/cart/cart.webhook.ts
@@ -32,51 +32,47 @@ const modify = async ({ body, shop }: CreateProps) => {
   await CartModel.deleteMany({ cartId: body.id, shop });
 
   const lineItems = body.line_items;
-  return Promise.all(
-    lineItems.map(async (lineItem) => {
-      const productId = lineItem.product_id.toString();
-      const _data = lineItem.properties?._data;
-      if (_data) {
-        const data: Data = JSON.parse(_data.split("\\").join(""));
-        const { timeZone, start, end, staff } = data;
+  const models = [];
 
-        const response = await widgetController.availabilityDay({
-          query: {
-            staffId: staff.staff,
-            date: start.substring(0, 10),
-            productId,
-            shop,
-          },
+  for (const lineItem of lineItems) {
+    const productId = lineItem.product_id.toString();
+    const _data = lineItem.properties?._data;
+    if (_data) {
+      const data: Data = JSON.parse(_data.split("\\").join(""));
+      const { start, end, staff } = data;
+
+      const response = await widgetController.availabilityDay({
+        query: {
+          staffId: staff.staff,
+          date: start.substring(0, 10),
+          productId,
+          shop,
+        },
+      });
+
+      const validateDate = !!response.find(
+        (scheduleDate) =>
+          scheduleDate.date === start.substring(0, 10) &&
+          scheduleDate.hours.find(
+            (hour) =>
+              hour.start.toISOString() === start &&
+              hour.end.toISOString() === end
+          )
+      );
+
+      if (validateDate) {
+        models.push({
+          cartId: body.id,
+          start,
+          end,
+          staff: new mongoose.Types.ObjectId(staff.staff),
+          shop,
         });
-
-        const validateDate = !!response.find(
-          (scheduleDate) =>
-            scheduleDate.date === start.substring(0, 10) &&
-            scheduleDate.hours.find(
-              (hour) =>
-                hour.start.toISOString() === start &&
-                hour.end.toISOString() === end
-            )
-        );
-
-        if (validateDate) {
-          const update = {
-            cartId: body.id,
-            start,
-            end,
-            staff: new mongoose.Types.ObjectId(staff.staff),
-            shop,
-          };
-
-          return await CartModel.updateOne(
-            { cartId: body.id },
-            { $set: update },
-            { upsert: true }
-          );
-        }
       }
-    })
-  );
+    }
+  }
+
+  await CartModel.insertMany(models);
 };
 
 export default { modify };
