0028c62744e74dd5750dd4b2f3cc4b089f898a1b
Jamal Soueidan <jamal@soueidan.com>
2022-12-22 23:46:08 +0300

    fix sonar issue

--- web/frontend/libs/validators/isPhoneNumber.ts
+++ web/frontend/libs/validators/isPhoneNumber.ts
@@ -1,6 +1,6 @@
 export default (error: string): ((input: string) => string | undefined) => {
   return (input: string) => {
-    const filter = /^[+]?[(]?[\d]{3}[)]?[-\s.]?[\d]{3}[-\s.]?[\d]{4,6}$/im;
+    const filter = /^(\+|\d)[0-9]{7,16}$/im;
     if (!filter.test(input)) {
       return error;
     }
