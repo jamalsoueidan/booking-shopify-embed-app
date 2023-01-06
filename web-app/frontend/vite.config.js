34488594cfdafe21557ad7aeaab88a74125d1363
Jamal Soueidan <jamal@soueidan.com>
2023-01-06 15:28:57 +0300

    create dev and prod env

--- web-app/frontend/vite.config.js
+++ web-app/frontend/vite.config.js
@@ -1,14 +1,27 @@
 import react from "@vitejs/plugin-react";
-import { dirname } from "path";
+import path from "path";
 import { visualizer } from "rollup-plugin-visualizer";
-import { fileURLToPath } from "url";
 import { defineConfig, splitVendorChunkPlugin } from "vite";
 import checker from "vite-plugin-checker";
 import tsconfigPaths from "vite-tsconfig-paths";
-import dynamicImportVars from "@rollup/plugin-dynamic-import-vars";
 
-// https://vitejs.dev/config/
+const proxyOptions = {
+  target: `http://127.0.0.1:8000`,
+  changeOrigin: false,
+  secure: true,
+  ws: false,
+  configure: (proxy) => {
+    proxy.on("error", (err) => {
+      console.log("proxy error", err);
+    });
+  },
+};
+
 export default defineConfig({
+  root: path.join(__dirname, "."),
+  build: {
+    sourcemap: process.env.NODE_ENV !== "production",
+  },
   plugins: [
     react(),
     checker({ typescript: true }),
@@ -16,10 +29,12 @@ export default defineConfig({
     visualizer(),
     tsconfigPaths(),
   ],
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: "./main.tsx",
+  server: {
+    host: "localhost",
+    port: 3000,
+    proxy: {
+      "^/(\\?.*)?$": proxyOptions,
+      "^/api(/|(\\?.*)?$)": proxyOptions,
     },
   },
 });
--- web-app/index.ts
+++ web-app/index.ts
@@ -2,26 +2,46 @@ import express from "express";
 import path from "path";
 import assetsRouter from "./assets-router";
 
-const app = express();
-
-app.use("/", assetsRouter);
-
-app.get("/api/v1", (req, res) => {
-  res.json({
-    project: "React and Express Boilerplate",
-    from: "Vanaldito",
-  });
-});
-
-app.get("/*", (_req, res) => {
-  res.sendFile(path.join(__dirname, "..", "frontend", "index.html"));
-});
-
 const { PORT = 8000 } = process.env;
 
-app.listen(PORT, () => {
-  console.log();
-  console.log(`  App running in port ${PORT}`);
-  console.log();
-  console.log(`  > Local: \x1b[36mhttp://localhost:\x1b[1m${PORT}/\x1b[0m`);
-});
+const DEV_INDEX_PATH = `${process.cwd()}/frontend/`;
+const PROD_INDEX_PATH = `${process.cwd()}/frontend/dist/`;
+
+export async function createServer(
+  root = process.cwd(),
+  isProd = process.env.NODE_ENV === "production"
+) {
+  const app = express();
+
+  if (isProd) {
+    app.use("/", express.static(PROD_INDEX_PATH));
+  } else {
+    app.use("/", assetsRouter);
+  }
+
+  app.get("/api/v1", (req, res) => {
+    res.json({
+      project: "React and Express Boilerplate",
+      from: "Vanaldito",
+    });
+  });
+
+  app.get("/*", (_req, res) => {
+    const htmlFile = path.join(
+      isProd ? PROD_INDEX_PATH + "index.html" : DEV_INDEX_PATH + "dev.html"
+    );
+
+    res.sendFile(htmlFile);
+  });
+
+  return { app };
+}
+
+createServer().then(({ app }) =>
+  app.listen(PORT, () => {
+    console.log();
+    console.log(`  App running in port ${PORT}`);
+    console.log();
+    console.log(`  > Local: \x1b[36mhttp://localhost:\x1b[1m${PORT}/\x1b[0m`);
+  })
+);
