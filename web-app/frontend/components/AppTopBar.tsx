158d128f2292e4e6e6699a1ffbe118b935d66f90
Jamal Soueidan <jamal@soueidan.com>
2023-01-06 13:45:58 +0300

    remove search

--- web-app/frontend/components/AppTopBar.tsx
+++ web-app/frontend/components/AppTopBar.tsx
@@ -1,20 +1,8 @@
-import { ActionList, TopBar } from "@shopify/polaris";
+import { TopBar } from "@shopify/polaris";
 import { useCallback, useState } from "react";
 
 export const AppTopBar = ({ toggleNavigation }: any) => {
-  const [searchActive, setSearchActive] = useState(false);
-  const [searchValue, setSearchValue] = useState("");
   const [userMenuActive, setUserMenuActive] = useState(false);
-
-  const handleSearchResultsDismiss = useCallback(() => {
-    setSearchActive(false);
-    setSearchValue("");
-  }, []);
-
-  const handleSearchFieldChange = useCallback((value: any) => {
-    setSearchValue(value);
-    setSearchActive(value.length > 0);
-  }, []);
 
   const toggleUserMenuActive = useCallback(
     () => setUserMenuActive((userMenuActive) => !userMenuActive),
@@ -45,31 +33,10 @@ export const AppTopBar = ({ toggleNavigation }: any) => {
     />
   );
 
-  const searchResultsMarkup = (
-    <ActionList
-      items={[
-        { content: "Shopify help center" },
-        { content: "Community forums" },
-      ]}
-    />
-  );
-
-  const searchFieldMarkup = (
-    <TopBar.SearchField
-      onChange={handleSearchFieldChange}
-      value={searchValue}
-      placeholder="Search"
-    />
-  );
-
   return (
     <TopBar
       showNavigationToggle
       userMenu={userMenuMarkup}
-      searchResultsVisible={searchActive}
-      searchField={searchFieldMarkup}
-      searchResults={searchResultsMarkup}
-      onSearchResultsDismiss={handleSearchResultsDismiss}
       onNavigationToggle={toggleMobileNavigationActive}
     />
   );
