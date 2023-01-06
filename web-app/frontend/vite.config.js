e12e7b321e06dcad70f1b1c58e869711267a05f4
Jamal Soueidan <jamal@soueidan.com>
2023-01-06 11:54:38 +0300

    update frontend to work with typescript

--- web-app/frontend/vite.config.js
+++ web-app/frontend/vite.config.js
@@ -1,13 +1,25 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { dirname } from "path";
+import { visualizer } from "rollup-plugin-visualizer";
+import { fileURLToPath } from "url";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
+import checker from "vite-plugin-checker";
+import tsconfigPaths from "vite-tsconfig-paths";
+import dynamicImportVars from "@rollup/plugin-dynamic-import-vars";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({ typescript: true }),
+    splitVendorChunkPlugin(),
+    visualizer(),
+    tsconfigPaths(),
+  ],
   build: {
     manifest: true,
     rollupOptions: {
-      input: "./main.jsx",
+      input: "./main.tsx",
     },
   },
 });
